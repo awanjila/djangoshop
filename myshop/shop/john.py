872f356e2de927189e19d9de554a2018c8a28945
awanjila <abemuchikan@gmail.com>
2017-12-27 16:38:37 +0300

    first commit

---
+++ myshop/shop/john.py
@@ -0,0 +1,8 @@
+def level_drunkness(drunkness):
+	drunkness=0
+	if drunkness>=5:
+		return print("john amelewa")
+	elif drunkness<5:
+		return print("bado john hajalewa")
+	else:
+		return print("endeni mkanunue pombe")
